f7e5afdfbe4346e0d9146b03fa07ab704e25f581
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2018-05-19 15:10:17 +0200

    change colors in sharpness plot

--- private/plot_sharpness.m
+++ private/plot_sharpness.m
@@ -30,7 +30,7 @@ function [h, x_light, y_light, x_skip, y_skip]=plot_sharpness(images, current)
   % h2=plot(x_dark, y_dark,  'ks');
   % h3=plot(x_flat, y_flat,  'mv');
   h2 = []; h3 = [];
-  h4=plot(x_skip, y_skip,  'gx');
+  h4=plot(x_skip, y_skip,  'rs');
   if current >=1 && current <= numel(images)
     h5=plot(current, y(current), 'c+'); set(h5, 'MarkerSize',10);
   else h5=[];
@@ -46,7 +46,7 @@ function [h, x_light, y_light, x_skip, y_skip]=plot_sharpness(images, current)
   hold off
   xlabel('Image index')
   ylabel('Sharpness (higher is better)');
-  title('Sharpness: {\color{blue}Left-clik}:SKIP {\color{blue}right-click}:LIGHT {\color{blue}shift-click}:OPEN {\color{red}}:EXIT');
+  title('Sharpness: {\color{blue}Left-clik}:SKIP {\color{blue}right-click}:LIGHT {\color{blue}shift-click}:OPEN {\color{red}ESC}:EXIT');
   h = [ h1, h2, h3, h4, h5 ];
   legend(h, leg);
   xlim([ 0 numel(images)+1 ]); ylim('auto');
